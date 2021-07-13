b8e52efda92d1ccda45b291b17a15562edd7a921
wdEffort <yoman3011@gmail.com>
2021-07-14 00:11:12 +0900

    Validation V2 - BindingResult를 사용한 검증 처리(2 / 입력 값 유지하기)

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -50,7 +50,7 @@ public class ValidationItemControllerV2 {
      * @param model
      * @return
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
@@ -73,6 +73,46 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 //글로벌 오류: new ObjectError(""@ModelAttribute 이름", "오류 기본 메시지")
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        //검증에 실패하면 다시 입력폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            //필드 오류: new FieldError("@ModelAttribute 이름", "오류가 발생한 필드명", "실패한 값", "타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분 값", "메시지 코드", "메시지에서 사용하는 인자", "오류 기본 메시지")
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000원 까지 허용합니다."));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                //글로벌 오류: new ObjectError(""@ModelAttribute 이름", "메시지 코드", "메시지에서 사용하는 인자", "오류 기본 메시지")
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
