1b84937d3b55f18adf3c16ab4ea5a3fd31be3b61
wdEffort <yoman3011@gmail.com>
2021-07-18 22:10:40 +0900

    Bean Validation - Spring Boot Validation 설정, 테스트 코드 작성

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank //빈값 + 공백만 있는 경우를 허용하지 않음.
     private String itemName;
+
+    @NotNull //NULL을 허용하지 않음.
+    @Range(min = 1000, max = 1000000) //범위 안의 값이어야함.
     private Integer price;
+
+    @NotNull
+    @Max(9999) //최대 9999까지만 허용.
     private Integer quantity;
 
     public Item() {
