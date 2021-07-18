4ca10a6d4447cde428dc189d5536742307624fd3
wdEffort <yoman3011@gmail.com>
2021-07-18 22:40:53 +0900

    Bean Validation - 오브젝트 오류 설정

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,20 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+/**
+ * 오브젝트 오류 설정
+ * 실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우들도 있기 때문에
+ * 오브젝트 오류(글로벌 오류)의 경우 @ScriptAssert를 억지로 사용하는 것보다는
+ * 오브젝트 오류 관련 부분만 직접 자바 코드로 작성하는 것을 권장함.
+ */
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10,000원 넘게 입력해 주세요.")
 public class Item {
 
     private Long id;
